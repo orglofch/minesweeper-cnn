b77a43aa55a482b1d8a319129069bafdadf9a08a
Owen Glofcheski <orglofch@google.com>
2021-12-22 21:21:11 -0500

    Add support for flagging cells to the CLI

--- main.py
+++ main.py
@@ -1,33 +1,77 @@
 import minesweeper
 
+def print_usage():
+    """Prints a usage error message to stdout."""
+    print("""
+    Commands:
+       sweep <x> <y> - Sweep a location for mines.
+       flag <x> <y>  - Flag a location as being a mine.
+       exit          - Exit the game
+    """);
+
+def run_sweep(input_pieces: list[str],
+              field: minesweeper.Field) -> minesweeper.FieldState:
+    """Runs the 'sweep' command.
+
+    Raises:
+      ValueError: If input_pieces is malformed.
+    """
+    x = int(input_pieces[1])
+    y = int(input_pieces[2])
+    return field.Sweep(x, y)
+
+def run_flag(input_pieces: list[str],
+             field: minesweeper.Field):
+    """Runs the 'flag' command.
+
+    Raises:
+      ValueError: If input_pieces is malformed.
+    """
+    x = int(input_pieces[1])
+    y = int(input_pieces[2])
+    field.Flag(x, y)
+
 def print_field(field: minesweeper.Field):
+    """Prints a field to stdout."""
     for i, row in enumerate(field.proximity):
         for j, value in enumerate(row):
-            if not field.mask[i][j]:
-                print(' ', end=' ')
+            mask = field.mask[i][j]
+            symbol: str = None
+            if mask == 0:
+                symbol = '_'
+            elif mask == 2:
+                symbol = 'P'
             elif value == -1:
-                print('*', end=' ')
+                symbol = '*'
             else:
-                print(value, end=' ')
+                symbol = value
+            print(symbol, end=' ')
         print('')
 
 def main():
     field = minesweeper.Field(100, 50, 500)
 
-    while True:
-        x = None
-        y = None
-        try:
-            coords = input("Enter coordinates 'x y':")
-            x, y = [int(v) for v in coords.split(' ')]
-        except ValueError:
-            print('Invalid input')
-            continue
-        if not 0 <= x < field.width or not 0 <= y < field.height:
-            print('Invalid coordinates')
-            continue;
+    field_state = minesweeper.FieldState.UNSOLVED
+    while field_state == minesweeper.FieldState.UNSOLVED:
+        input_pieces = input("Enter command: ").split(' ')
+        if not input_pieces:
+            print('Invalid command')
+            print_usage()
 
-        field_state = field.Sweep(x, y)
+        try:
+            if input_pieces[0] == 'sweep':
+                field_state = run_sweep(input_pieces, field)
+            elif input_pieces[0] == 'flag':
+                run_flag(input_pieces, field)
+            elif input_pieces[0] == 'exit':
+                return
+            else:
+                print_usage()
+                continue
+        except ValueError:
+            print_usage()
+            continue
+
         print_field(field)
         if field_state == minesweeper.FieldState.FAILED:
             print('Game over')
--- minesweeper.py
+++ minesweeper.py
@@ -13,16 +13,18 @@ class Field:
     Attributes:
       width: The width of the field.
       height: The height of the field.
-      mask: A 2D array indicating revealed portions of the field. False indicates a cell
-            is hidden. True indicates a cell is revealed.
-      proximity: A 2D array indicating proximity to mines (e.g. a value of 2 indicates
-                 2 adjacent mines). -1 indicates a mine.
+      mask: A 2D array indicating revealed portions of the field. 0 indicates a cell
+            is hidden. 1 indicates a cell is revealed. 2 indicates a cell is flagged.
+      proximity: A 2D array indicating proximity to mines. Positive values indicate
+                 adjacent mines. -1 indicates a mine.
+      state: The current state of the field.
     """
     def __init__(self, width: int, height: int, num_mines: int):
         self.width = width
         self.height = height
-        self.mask = numpy.zeros((height, width), numpy.bool_)
+        self.mask = numpy.zeros((height, width), numpy.int8)
         self.proximity = numpy.zeros((height, width), numpy.int8)
+        self.state = FieldState.UNSOLVED
 
         # Generate mines.
         for _ in range(num_mines):
@@ -47,12 +49,21 @@ class Field:
         Returns:
           The state of the field after the sweep.
         """
-        proximity = self.proximity[y][x];
+        mask = self.mask[y][x]
+        proximity = self.proximity[y][x]
+
+        if self.state == FieldState.SOLVED or self.state == FieldState.FAILED:
+            # The field is already in a finished state.
+            return self.state
+        if mask == 2:
+            # Flags can't be clicked.
+            return self.state
+
         if proximity == -1:
             # A mine has been hit.
             self.mask[y][x] = 1
-            return FieldState.FAILED
-        elif not self.mask[x][y]:
+            self.state = FieldState.FAILED
+        elif mask == 0:
             # Expand the mask via flood-fill.
             queue = [(x, y)]
             visited = numpy.zeros((self.height, self.width), numpy.bool_)
@@ -69,12 +80,23 @@ class Field:
                             if 0 <= nx < self.width and 0 <= ny < self.height and self.proximity[ny][nx] != -1 and not visited[ny][nx]:
                                 queue.append((nx, ny))
 
-        solved = True
-        for i, row in enumerate(self.proximity):
-            for j, value in enumerate(row):
-                if value != -1 and not self.mask[i][j]:
-                    solved = False
+            solved = True
+            for i, row in enumerate(self.proximity):
+                for j, value in enumerate(row):
+                    if value != -1 and self.mask[i][j] == 0:
+                        solved = False
+                        break
+                if not solved:
                     break
-            if not solved:
-                break
-        return FieldState.SOLVED if solved else FieldState.UNSOLVED
+            if solved:
+                self.state = FieldState.SOLVED
+        return self.state
+
+    def Flag(self, x: int, y: int):
+        """Flags a cell as a mine."""
+        mask = self.mask[y][x]
+
+        if mask == 0:
+            self.mask[y][x] = 2
+        elif mask == 2:
+            self.mask[y][x] = 0
